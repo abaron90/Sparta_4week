a193cf355ba4b508063d8167783d8ac9306e453c
Kim <abaon90@nate.com>
2022-10-13 18:54:07 +0900

    MobileSoftware - Java

---
+++ Sparta_4week/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import TodoList from "../src/js/pages/TodoList";
+
+function App() {
+  return <TodoList />;
+}
+
+export default App;
+
+// const StFormContainer = styled.div`
+//   display: flex;
+//   gap: 24px;
+//   padding: 30px;
+// `;
+
+// const StButton = styled.button`
+//   border: none;
+//   background-color: #eee;
+//   height: 25px;
+//   cursor: pointer;
+//   width: 120px;
+//   border-radius: 12px;
+// `;
+
+// const StInput = styled.input`
+//   border: 1px solid #eee;
+//   margin: 0 24px;
+//   height: 25px;
+//   width: 300px;
+//   border-radius: 12px;
+//   outline: none;
+//   padding: 0 10px;
+// `;
